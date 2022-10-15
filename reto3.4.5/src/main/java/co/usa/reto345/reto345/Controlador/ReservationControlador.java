8116630a11a9a90593da200971736b894cbba2c5
jorge rincón <tecrinconj@gmail.com>
2022-10-15 12:05:53 -0500

    subida

---
+++ reto3.4.5/src/main/java/co/usa/reto345/reto345/Controlador/ReservationControlador.java
@@ -0,0 +1,48 @@
+package co.usa.reto345.reto345.Controlador;
+
+
+import co.usa.reto345.reto345.modelo.Reservation;
+import co.usa.reto345.reto345.service.ReservationServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/Reservation")
+public class ReservationControlador {
+
+    @Autowired
+    private ReservationServicio reservationService;
+    @GetMapping("/all")
+    public List<Reservation> getReservation(){
+        return reservationService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Reservation> getReservation(@PathVariable("id") int reservationId) {
+        return reservationService.getReservation(reservationId);
+    }
+
+    @PostMapping("/save")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Reservation save(@RequestBody Reservation reservation) {
+        return reservationService.save(reservation);
+    }
+
+    @PutMapping("/update")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Reservation update(@RequestBody Reservation reservation) {
+        return reservationService.update(reservation);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public boolean delete(@PathVariable("id") int id){
+
+        return reservationService.deleteReservation(id);
+    }
+
+}
